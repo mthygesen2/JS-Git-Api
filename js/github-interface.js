e036506a746f36fe30b4855499a9045a876a3771
Mao Thygesen <maothygesen@gmail.com>
2016-03-18 10:21:18 -0700

    add list for repo name and description

--- js/github-interface.js
+++ js/github-interface.js
@@ -4,22 +4,10 @@ var apiKey = require('./../.env').apiKey;
 
 
 $(document).ready(function() {
-    // getRepos();
     $('#userInfo').submit(function(event) {
       event.preventDefault();
       userName = $('#userName').val();
       $('.showName').text(userName);
+      getRepos();
     });
   });
-
-
-
-// $(document).ready(function() {
-//     $('#userName').val(""); //clears userName input
-//     $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
-//       console.log(response);
-//     })  .fail(function(error) {
-//         $('.showName').text(error.message);
-//   });
-// });
-// });
--- js/github.js
+++ js/github.js
@@ -1,10 +1,27 @@
 var apiKey = require('./../.env').apiKey;
 
 
-// exports.getRepos = function(){
-//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
+exports.getRepos = function(){
+  $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+    for(var i = 0; i < response.length; i ++) {
+
+    $('.showRepo').append(
+
+      "<li>" + "<a href=" + response[i].html_url + ">" + response[i].name + "</a></li>" +
+      "<ul><li>" + response[i].description + "</li></ul>"
+    );
+  }
+    console.log(response);
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+};

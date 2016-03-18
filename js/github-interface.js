5f240919fac86e198692f3cb7ceaad980241f3b9
Mao Thygesen <maothygesen@gmail.com>
2016-03-18 10:43:28 -0700

    change default repo to show more than 30

--- js/github-interface.js
+++ js/github-interface.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
     $('#userInfo').submit(function(event) {
       event.preventDefault();
       userName = $('#userName').val();
-      $('.showName').text(userName);
       getRepos();
     });
   });
+
+
+  // $('.showName').text(userName);
--- js/github.js
+++ js/github.js
@@ -3,19 +3,22 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(){
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
-
+      console.log(response);
+    $('.showName').append (
+      "<h1>" + response.name + "</h1>" +
+      "<h4>" + response.login + "</h4>" +
+      "<h4>" + response.location + "</h4>"
+    );
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 
-  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey + '&per_page=9999').then(function(response){
     for(var i = 0; i < response.length; i ++) {
 
     $('.showRepo').append(
-
-      "<li>" + "<a href=" + response[i].html_url + ">" + response[i].name + "</a></li>" +
-      "<ul><li>" + response[i].description + "</li></ul>"
+      "<li>" + "<a href=" + response[i].html_url + ">" + response[i].name + "</a>" +
+      "<p>" + response[i].description + "</p></li>"
     );
   }
     console.log(response);
